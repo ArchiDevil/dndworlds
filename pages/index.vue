8fd7a2e88040985a0ee177717d1d59e6fe5e787a
Denis Bezykornov <archidevil52@gmail.com>
2023-09-13 23:36:26 +0300

    First integration of Nuxt

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  data() {
+    return {
+      message: 'Hello world!',
+    }
+  },
+})
+</script>
+
+<template>
+  <h1>Some example website</h1>
+  <a href="/books">Books catalogue</a>
+  <a href="/blog">Blog with posts</a>
+</template>
+
+<style scoped>
+a {
+  display: block;
+  margin-bottom: 10px;
+}
+</style>
