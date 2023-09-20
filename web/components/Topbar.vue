46a2e8883b77fad12a43984bf66edf53d1ec63ab
Denis Bezykornov <archidevil52@gmail.com>
2023-09-21 00:04:42 +0300

    Add Layout with top bar

---
+++ web/components/Topbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="my-12 flex flex-row-reverse">
+    <NuxtLink class="mx-10" to="/books">Каталог материалов</NuxtLink>
+    <NuxtLink class="mr-10" to="/">Блог</NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+a {
+  @apply text-slate-800 font-semibold text-xl;
+}
+
+a:hover {
+  @apply text-slate-900 underline underline-offset-4 decoration-2;
+}
+</style>
---
+++ web/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container px-12">
+    <Topbar></Topbar>
+    <slot />
+  </div>
+</template>
+
+<style>
+body {
+  @apply bg-slate-50;
+}
+</style>
