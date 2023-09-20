27a33869285ebc95aded93455b6eef0b0eada6a1
Denis Bezykornov <archidevil52@gmail.com>
2023-09-21 00:48:14 +0300

    Add book and chapter page placeholders

---
+++ web/pages/book-[id]/chapter-[cid].vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const route = useRoute()
+const id = ref(route.params.cid)
+</script>
+
+<template>
+  <p>This a chapter {{ id }} content</p>
+</template>
---
+++ web/pages/book-[id]/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const route = useRoute()
+const id = ref(route.params.id)
+</script>
+
+<template>
+  <h1>This is a book page</h1>
+  <p>Its ID is: {{ id }}</p>
+</template>
