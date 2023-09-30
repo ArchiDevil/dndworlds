58db15980090ae9776763a0dd902c11b1fd0308b
Denis Bezykornov <archidevil52@gmail.com>
2023-09-30 15:10:37 +0300

    Better styles for formatted document

--- web/pages/book-[id]/chapter-[cid].vue
+++ web/pages/book-[id]/chapter-[cid].vue
@@ -29,13 +29,57 @@ onMounted(() => {
 <style scoped>
 .container-content :deep(h1) {
   font-size: 2rem;
-  font-weight: bold;
+  font-weight: 600;
   margin: 1rem 0;
 }
 
 .container-content :deep(h2) {
   font-size: 1.5rem;
-  font-weight: bold;
+  font-weight: 600;
   margin: 1rem 0;
+  border-bottom: 2px solid #aaa;
+}
+
+.container-content :deep(h3) {
+  font-size: 1.2rem;
+  font-weight: 600;
+  margin: 1rem 0;
+}
+
+.container-content :deep(h6) {
+  font-size: 1.2rem;
+  font-weight: 600;
+  margin: 0.5rem 0 0 0;
+}
+
+.container-content :deep(blockquote) {
+  border: 2px solid #aaa;
+  padding: 0.5rem;
+  margin: 0.5rem 0;
+}
+
+.container-content :deep(ul) {
+  margin: 0.5rem 0;
+  padding-left: 1rem;
+}
+
+.container-content :deep(li) {
+  list-style: outside;
+  list-style-type: disc;
+}
+
+.container-content :deep(th) {
+  border: 1px solid #aaa;
+  padding: 0.25rem 0.5rem;
+  text-align: left;
+}
+
+.container-content :deep(td) {
+  border: 1px solid #aaa;
+  padding: 0.25rem 0.5rem;
+}
+
+.container-content :deep(p) {
+  margin: 0.5rem 0;
 }
 </style>
